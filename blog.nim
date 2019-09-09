b20edc4ff6912ef60a3e90b4be771c22f2f4e087
ZhaoLiang <176405269@qq.com>
2019-09-09 17:09:03 +0800

    生成列表页

--- blog.nim
+++ blog.nim
@@ -13,78 +13,111 @@ type
 
 const markdown_path = "./markdown/" # Markdown文件路径
 const html_path = "./html/" # 生成的HTML文件路径
-const page_size = 10 #分页大小
+const page_size = 3 #分页大小
 
 var templates = newNwt("template/*.html") # 模板引擎
-#var markdown_files: seq[string]
-var blogs: seq[Blog]
+var categories: seq[string] # 所有分类
+var blogs: seq[Blog] # 所有博客
 
 # 从Markdown文件获取博客信息
 proc init_blog_form_md(file: string): Blog =
     var num = 0
     result.file = file
+    result.category = splitFile(file).dir
     var f = markdown_path & file
     for line in f.open().lines:
-        num += 1
-        if num < 6:
-            var start_position = find(line, re"\(")
-            var end_position = find(line, re"\)")
-            if start_position > -1 and end_position > -1:
-                var value = line[start_position + 1..end_position - 1]
-                if num == 1: result.title = value
-                if num == 2: result.category = value
-                if num == 3: result.date = value
-                if num == 4: result.keywords = value
-                if num == 5: result.summary = value
-    #echo result
+        if num == 0: result.title = line
+        if num == 1: result.date = line
+        if num == 2: result.keywords = line
+        if num == 3: result.summary = line
+        if num < 4: num += 1
 
 # 获取Markdown文件
-proc get_markdown_files() =
+proc init_markdown_files() =
     for file in walkDirRec(markdown_path, relative = true):
         if file.endsWith ".md":
-            #markdown_files.add file
+            var meta = splitFile(file)
+            if not categories.contains meta.dir: categories.add meta.dir # 分类
             var blog = init_blog_form_md(file)
             blogs.add blog
 
+# 生成博客列表内容
+proc gen_blog_list_html(list_blogs: seq[Blog]): seq[string] =
+    for item in list_blogs:
+        var meta = splitFile(item.file)
+        var context = %* {
+            "title": item.title,
+            "category": item.category,
+            "date": item.date,
+            "keywords": item.keywords,
+            "summary": item.summary,
+            "file": meta.dir & "/" & meta.name & ".html"
+        }
+        var html = templates.renderTemplate("list-item.html", context)
+        result.add html
+
+# 生成博客列表文件
+proc gen_list_file(list_blogs: seq[Blog], category: string = "") =
+    # TODO 生成首页异常
+    var total_page = list_blogs.len div page_size # 总页数
+    if list_blogs.len mod page_size > 0: total_page += 1
+    for page in 1..total_page:
+        var begin = page * page_size - page_size
+        var number = if page == total_page: list_blogs.len mod page_size else: page_size
+        var list_html = gen_blog_list_html(list_blogs[begin..begin + number - 1])
+        var context = %* {
+            "list": list_html.join(""),
+            "preview": if page > 1: """<a href="index$1.html">上一页</a>""" % [if page == 2: "" else: "-" & $(page - 1)] else: "",
+            "next": if page < total_page: """<a href="index-$1.html">下一页</a>""" % [$(page + 1)] else: "",
+            "page": page
+        }
+        var html = templates.renderTemplate("list.html", context)
+        var target_dir = html_path & category
+        if not existsDir(target_dir): createDir(target_dir)
+        writeFile(target_dir & "/index" & (if page > 1: "-" & $(page) else : "") & ".html", html)
+        
+
 # 生成首页
-proc gen_index() =
-    #var total_page: int = markdown_files.len div page_size
-    #if markdown_files.len mod page_size > 0: total_page += 1
-    #echo "total page: ", total_page
-    #echo "len: ", markdown_files.len
-    #for i in 1..total_page:
-    var list: seq[string]
-    for item in blogs:
-        var html_item = """<li>
-            <a class="title mb-10" href="/$1/$2.html">$2</a>
-            <div class="info mb-10">分类：<a href="/$1">$1</a> 日期：<span>$3</span></div>
-            <div class="summary mb-10">$4</div>
-            <div class="keywords">$5</div>
-        </li>""" % [item.category, item.title, item.date, item.summary, item.keywords]
-        list.add html_item
-    #echo list
-    var context = %* {
-        "list": list.join("")
-    }
-    var html = templates.renderTemplate("index.html", context)
-    writeFile(html_path & "index.html", html)
+proc gen_index_page() =
+    gen_list_file(blogs)
+    echo "Generated index page"
+
+# 生成博客列表页
+proc gen_list_page() =
+    for c in categories:
+        var list_blogs: seq[Blog]
+        for blog in blogs:
+            if blog.category == c: list_blogs.add blog
+        gen_list_file(list_blogs, category = c)
+        echo "Generated list pages of ", c
 
 # 生成博客页
-proc gen_blog() =
+proc gen_detail_page() =
     for item in blogs:
         var file = markdown_path & item.file
-        var html_content: string
+        var lines: seq[string]
+        var num = 0
         for line in file.open().lines:
-            html_content.add markdown(line)
+            if num > 4: lines.add line
+            num += 1
+        var html_content = markdown(lines.join("\n"))
         var context = %* {
-            "content": html_content
+            "content": html_content,
+            "title": item.title,
+            "category": item.category,
+            "date": item.date,
+            "keywords": item.keywords,
+            "summary": item.summary
         }
-        var html = templates.renderTemplate("blog.html", context)
+        var html = templates.renderTemplate("detail.html", context)
         var file_meta = splitFile(item.file)
         var target_dir = html_path & file_meta.dir
         if not existsDir(target_dir): createDir(target_dir)
-        writeFile(target_dir & "/" & file_meta.name & ".html", html)
+        var target_file = target_dir & "/" & file_meta.name & ".html"
+        writeFile(target_file, html)
+        echo "Generated blog page of ", target_dir & "/" & file_meta.name & ".html"
 
-get_markdown_files()
-gen_index()
-gen_blog()
+init_markdown_files()
+gen_index_page()
+gen_list_page()
+gen_detail_page()
