3cbbf0eb4f7ff4c1acbfbdabd592e58b4e10460b
ZhaoLiang <176405269@qq.com>
2019-09-25 15:07:01 +0800

    proc采用驼峰命名，生成html前先清空已有文件

--- blog.nim
+++ blog.nim
@@ -20,7 +20,7 @@ var categories: seq[string] # 所有分类
 var blogs: seq[Blog] # 所有博客
 
 # 从Markdown文件获取博客信息
-proc init_blog_form_md(file: string): Blog =
+proc initBlogFromMd(file: string): Blog =
     var num = 0
     result.file = file
     result.category = splitFile(file).dir
@@ -32,23 +32,30 @@ proc init_blog_form_md(file: string): Blog =
         if num == 3: result.summary = line
         if num < 4: num += 1
 
-proc sort_blog(x, y: Blog): int =
+# 按时间倒序排列
+proc sortBlog(x, y: Blog): int =
     var a = parse(x.date, "yyyy-MM-dd")
     var b = parse(y.date, "yyyy-MM-dd")
     if a < b: -1 else: 1
 
+# 清空已有的html文件
+proc clearHTMLFile() = 
+    for file in walkDirRec(html_path):
+        if file.endsWith ".html":
+            discard tryRemoveFile(file)
+
 # 获取Markdown文件
-proc init_markdown_files() =
+proc initMdFile() =
     for file in walkDirRec(markdown_path, relative = true):
         if file.endsWith ".md":
             var meta = splitFile(file)
             if not categories.contains meta.dir: categories.add meta.dir # 分类
-            var blog = init_blog_form_md(file)
+            var blog = initBlogFromMd(file)
             blogs.add blog
-    blogs.sort(sort_blog, order = SortOrder.Descending)
+    blogs.sort(sortBlog, order = SortOrder.Descending)
 
 # 生成博客列表内容
-proc gen_blog_list_html(list_blogs: seq[Blog]): seq[string] =
+proc genBlogListHTML(list_blogs: seq[Blog]): seq[string] =
     for item in list_blogs:
         var meta = splitFile(item.file)
         var context = %* {
@@ -70,7 +77,7 @@ proc writeHtmlFile(source_file, target_file: string, params: JsonNode) =
     writeFile(target_file, html)
 
 # 生成博客列表文件
-proc gen_list_file(list_blogs: seq[Blog], category: string = "") =
+proc genListFile(list_blogs: seq[Blog], category: string = "") =
     var total_page = list_blogs.len div page_size # 总页数
     var m = list_blogs.len mod page_size # 余数
     if m > 0: total_page += 1
@@ -78,7 +85,7 @@ proc gen_list_file(list_blogs: seq[Blog], category: string = "") =
         var begin = page * page_size - page_size
         var number = page_size
         if page == total_page and m > 0: number = m
-        var list_html = gen_blog_list_html(list_blogs[begin..begin + number - 1])
+        var list_html = genBlogListHTML(list_blogs[begin..begin + number - 1])
         var context = %* {
             "title": if category.len  == 0: "首页" else: category,
             "list": list_html.join(""),
@@ -91,21 +98,21 @@ proc gen_list_file(list_blogs: seq[Blog], category: string = "") =
         writeHtmlFile("list.html", target_file, context)
         
 # 生成首页
-proc gen_index_page() =
-    gen_list_file(blogs)
+proc genIndexPage() =
+    genListFile(blogs)
     echo "Generated index page"
 
 # 生成博客列表页
-proc gen_list_page() =
+proc genListPage() =
     for c in categories:
         var list_blogs: seq[Blog]
         for blog in blogs:
             if blog.category == c: list_blogs.add blog
-        gen_list_file(list_blogs, category = c)
+        genListFile(list_blogs, category = c)
         echo "Generated list pages of ", c
 
 # 生成博客页
-proc gen_detail_page() =
+proc genDetailPage() =
     for item in blogs:
         var file = markdown_path & item.file
         var lines: seq[string]
@@ -127,7 +134,8 @@ proc gen_detail_page() =
         writeHtmlFile("detail.html", target_file, context)
         echo "Generated blog page of ", target_file
 
-init_markdown_files()
-gen_index_page()
-gen_list_page()
-gen_detail_page()
+initMdFile()
+clearHTMLFile()
+genIndexPage()
+genListPage()
+genDetailPage()
