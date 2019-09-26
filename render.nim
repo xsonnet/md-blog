67a67e600653eb192b545a3ccbc1c3679275d267
ZhaoLiang <176405269@qq.com>
2019-09-26 18:05:44 +0800

    加上文件

---
+++ render.nim
@@ -0,0 +1,32 @@
+import json, re, strutils
+
+type
+    View* = object
+        path*: string
+        global*: JsonNode
+
+# 处理include
+proc parseInclude(v: View, source: string): string =
+    let regexp = re"\{\%\s?include\s+.+\%\}"
+    let start = re"\{\%\s?include\s+"
+    let stop = re"\s?\%\}"
+    result = source
+    for m in source.findAll(regexp):
+        var file = m.replace(start).replace(stop)
+        result = result.replace(m, $(v.path & file).open().readAll())
+
+# 处理数据
+proc parseData(v: View, source: string, data: JsonNode): string = 
+    let regexp = re"\{{2}\s?\w+\b\s?\}{2}"
+    let start = re"\{{2}\s?"
+    let stop = re"\s?\}{2}"
+    result = source
+    for m in source.findAll(regexp):
+        var key = m.replace(start).replace(stop)
+        result = result.replace(m, data[key].getStr())
+
+proc render*(v: View, file: string, data: JsonNode = %* {}, uglify: bool = false): string = 
+    result = $(v.path & file).open().readAll()
+    result = v.parseInclude(result)
+    result = v.parseData(result, data)
+    if uglify: result = result.replace(re"\t|\n").replace(re"\s{2,}", " ")
