fc732a2b362469e44771c61fa4e5bf8179a993a7
ZhaoLiang <176405269@qq.com>
2019-12-31 15:46:52 +0800

    调试列表集合

--- app.nim
+++ app.nim
@@ -28,13 +28,18 @@ proc initMdFile(app: var App) =
 
 # 生成博客列表文件
 proc genListFile(app: App, list: seq[Blog], category: string = "") =
-    var totalPage = app.blogs.len div app.pageSize # 总页数
+    var totalPage = list.len div app.pageSize # 总页数
     var m = list.len mod app.pageSize # 余数
     if m > 0: totalPage += 1
     for page in 1..totalPage:
         var begin = page * app.pageSize - app.pageSize
         var number = app.pageSize
         if page == totalPage and m > 0: number = m
+        echo "总数量：", list.len
+        echo "栏目：", category
+        echo "页码：", page
+        echo "开始：", begin
+        echo "数量：", number
         let html = util.genBlogListHTML(list[begin..begin + number - 1])
         var context = %* {
             "title": if category.len  == 0: "首页" else: category,
@@ -55,7 +60,7 @@ proc genListPage(app: App) =
             if blog.category == item: list.add blog
         if list.len > 0:
             app.genListFile(list, category = item)
-            echo "Generated list pages of ", item
+            echo "生成列表页: ", item
 
 # 生成博客页
 proc genDetailPage(app: App) =
@@ -77,7 +82,7 @@ proc genDetailPage(app: App) =
         var meta = splitFile(item.file)
         var target_file = app.htmlPath & meta.dir & "/" & meta.name & ".html"
         writeHtmlFile("detail.html", target_file, context)
-        echo "Generated blog page of ", target_file
+        echo "生成博客内容: ", target_file
 
 proc main() =
     var app: App
